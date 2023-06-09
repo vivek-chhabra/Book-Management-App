a597486b9cfd6b952b8618b20e7a1b396bd4777c
vivek chhabra <vivekcb8642@gmail.com>
2023-06-09 19:30:26 +0530

    add App component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddBook from "./components/AddBook";
+import BookManager from "./components/BookManager";
+import NavBar from "./components/BookManager";
+
+function App() {
+    return (
+        <div className="App">
+            <BookManager/>
+        </div>
+    );
+}
+
+export default App;
